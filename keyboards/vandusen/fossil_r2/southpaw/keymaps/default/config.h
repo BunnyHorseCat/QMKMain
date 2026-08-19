11d180fa15566a86c2d12d9f609025d17571a48b
BunnyHorseCat <bunnyismsblog@gmail.com>
2026-08-19 10:44:09 -0400

    Add files to allow for VIA/VIAL compilation. config.h and rules.mk are not necessary when compiling for the default keymap.

---
+++ keyboards/vandusen/fossil_r2/southpaw/keymaps/default/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define VIAL_KEYBOARD_UID {0x07, 0x93, 0xA1, 0x7D, 0x91, 0x08, 0xDD, 0x7D}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#define VENDOR_ID       0x524D
+#define PRODUCT_ID      0x6061
---
+++ keyboards/vandusen/fossil_r2/standard/keymaps/default/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define VIAL_KEYBOARD_UID {0xA4, 0x17, 0x33, 0x2A, 0x6C, 0xA8, 0x91, 0x29}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#define VENDOR_ID       0x524D
+#define PRODUCT_ID      0x6060
